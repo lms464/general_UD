b1d932e6d6249132781dc61a5d49d3a7761e0a58
Liam Sharp <sharplm@deebo>
2022-08-19 14:53:19 -0400

    updates

--- CGTM_library/CGTM_Lag_Reweight.py
+++ CGTM_library/CGTM_Lag_Reweight.py
@@ -26,13 +26,13 @@ def cummulative_difference(pi_ref,pi_eq):
 
 def lag_cumDiff(lagmin,lagmax):
     import matplotlib.pyplot as plt
-    data = pd.read_csv("/home/liam/lms464/CG/data/states/low_short_inactive_tmpNEW.csv",index_col=0)#low_long_inactive_tmpNEW.csv",index_col=0)#"~/CG/data/states/low_short_inactive_tmp.csv",index_col=0)
+    data = pd.read_csv("/home/sharplm/CG/data/states/low_short_inactive_tmpNEW.csv",index_col=0)#low_long_inactive_tmpNEW.csv",index_col=0)#"~/CG/data/states/low_short_inactive_tmp.csv",index_col=0)
     dat_ind = ["%i"%i for i in range(int(len(data.T)/2),len(data.T))]
     dat_ind = ["0"] + dat_ind
     trj,n_states,remp = ta.remap_trajs(data[dat_ind].values)
     hist = np.histogram(trj,bins=n_states,range=(0,n_states))[0]
     hist = hist/hist.sum()
-    true_hist = pd.read_csv("/home/liam/lms464/CG/data/pi_eq_low_inactive_long_cgtmp_231.csv",index_col=0).values
+    true_hist = pd.read_csv("/home/sharplm/CG/data/pi_eq_low_inactive_long_cgtmp_231.csv",index_col=0).values
     true_hist = true_hist.T[0]
 
     its = 1
@@ -56,31 +56,77 @@ def lag_cumDiff(lagmin,lagmax):
 
         cum_diff_Short.append(cummulative_difference(hist_map,reweight_map))
         cum_diff_Long.append(cummulative_difference(true_hist,reweight_map))
-
-    plt.plot(np.arange(lagmin,lagmax,1),cum_diff_Short);plt.plot(np.arange(lagmin,lagmax,1),cum_diff_Long)
-    return cum_diff_Short,cum_diff_Long
+    np.savetxt("CGTM_Lag.dat",np.array([np.arange(lagmin,lagmax,1),cum_diff_Short,cum_diff_Long]).T)
+    # plt.plot(np.arange(lagmin,lagmax,1),cum_diff_Short);plt.plot(np.arange(lagmin,lagmax,1),cum_diff_Long)
+    #return cum_diff_Short,cum_diff_Long
 
 
 def window_cumDiff(stpsmin,stpsmax=None):
     import matplotlib.pyplot as plt
-    data = pd.read_csv("/home/liam/lms464/CG/data/states/low_short_inactive_tmpNEW.csv",index_col=0)#low_long_inactive_tmpNEW.csv",index_col=0)#"~/CG/data/states/low_short_inactive_tmp.csv",index_col=0)
+    data = pd.read_csv("/home/sharplm/CG/data/states/low_short_inactive_tmpNEW.csv",index_col=0)#low_long_inactive_tmpNEW.csv",index_col=0)#"~/CG/data/states/low_short_inactive_tmp.csv",index_col=0)
     dat_ind = ["%i"%i for i in range(int(len(data.T)/2),len(data.T))]
     dat_ind = ["0"] + dat_ind
     trj,n_states,remp = ta.remap_trajs(data[dat_ind].values)
     hist = np.histogram(trj,bins=n_states,range=(0,n_states))[0]
     hist = hist/hist.sum()
-    true_hist = pd.read_csv("/home/liam/lms464/CG/data/pi_eq_low_inactive_long_cgtmp_231.csv",index_col=0).values
+    true_hist = pd.read_csv("/home/sharplm/CG/data/pi_eq_low_inactive_long_cgtmp_231.csv",index_col=0).values
     true_hist = true_hist.T[0]
-    tau = 2
+    tau = 1
     its = 300
     # stps = 1
     out2 = []
     cum_diff_Long = []
     cum_diff_Short = []
-    for tau in np.linspace(1,3):
-        if stpsmax == None or stpsmax > len(trj.T[:,::tau])*2/3:
-            stpsmax = int(len(trj.T[:,::int(tau)])*2/3)
+    if stpsmax == None or stpsmax > len(trj.T[:,::tau])*2/3:
+        stpsmax = int(len(trj.T[:,::int(tau)])*2/3)
+    for stp in np.arange(stpsmin,stpsmax,1):
+        try:
+            reweighted_distributions, reweighted_matrices, eign = ta.optimized_resliced_voelz(
+                trj[:,::int(tau)],
+                its,
+                stp,
+                n_states,
+                return_eig=True,
+                return_matrices=True
+            )
+        except:
+            break
+        out2.append(eign)
+        reweight_mean = np.mean(reweighted_distributions,axis=0)
+        reweight_map = unmap(remp, reweight_mean, 231)
+        hist_map = unmap(remp,hist,231)
+
+        cum_diff_Short.append(cummulative_difference(hist_map,reweight_map))
+        cum_diff_Long.append(cummulative_difference(true_hist,reweight_map))
+
+    # plt.plot(np.arange(stpsmin,stpsmax,1),cum_diff_Short);
+    # plt.plot(np.arange(stpsmin,stpsmax,1),cum_diff_Long)
+    np.savetxt("CGTM_Lag.dat",np.array([np.arange(stpsmin,stpsmax,1),cum_diff_Short,cum_diff_Long]).T)
+    # return cum_diff_Short,cum_diff_Long
+
+def compair_Lag_window_cumDiff(lagmin, lagmax, stpsmin,stpsmax):
+    import matplotlib.pyplot as plt
+    data = pd.read_csv("/home/sharplm/CG/data/states/low_short_inactive_tmpNEW.csv",index_col=0)#low_long_inactive_tmpNEW.csv",index_col=0)#"~/CG/data/states/low_short_inactive_tmp.csv",index_col=0)
+    dat_ind = ["%i"%i for i in range(int(len(data.T)/2),len(data.T))]
+    dat_ind = ["0"] + dat_ind
+    trj,n_states,remp = ta.remap_trajs(data[dat_ind].values)
+    hist = np.histogram(trj,bins=n_states,range=(0,n_states))[0]
+    hist = hist/hist.sum()
+    true_hist = pd.read_csv("/home/sharplm/CG/data/pi_eq_low_inactive_long_cgtmp_231.csv",index_col=0).values
+    true_hist = true_hist.T[0]
+    # tau = 1
+    its = 300
+    # stps = 1
+    # out2 = []
+    # cum_diff_Long = []
+    # cum_diff_Short = []
+    # if stpsmax == None or stpsmax > len(trj.T[:,::tau])*2/3:
+    #     stpsmax = int(len(trj.T[:,::int(tau)])*2/3)
+    cum_diff_Short = pd.DataFrame(index=np.arange(stpsmin,stpsmax,1),columns=np.arange(lagmin, lagmax, 1))
+    cum_diff_Long = cum_diff_Short.copy()
+    for tau in np.arange(lagmin, lagmax, 1):
         for stp in np.arange(stpsmin,stpsmax,1):
+            print("tau:%i\twind:%i"%(int(tau),int(stp)))
             try:
                 reweighted_distributions, reweighted_matrices, eign = ta.optimized_resliced_voelz(
                     trj[:,::int(tau)],
@@ -90,20 +136,22 @@ def window_cumDiff(stpsmin,stpsmax=None):
                     return_eig=True,
                     return_matrices=True
                 )
+            # out2.append(eign)
+                reweight_mean = np.mean(reweighted_distributions,axis=0)
+                reweight_map = unmap(remp, reweight_mean, 231)
+                hist_map = unmap(remp,hist,231)
+
+                cum_diff_Short[tau][stp] = cummulative_difference(hist_map,reweight_map)
+                cum_diff_Long[tau][stp] = cummulative_difference(true_hist,reweight_map)
             except:
-                break
-            out2.append(eign)
-            reweight_mean = np.mean(reweighted_distributions,axis=0)
-            reweight_map = unmap(remp, reweight_mean, 231)
-            hist_map = unmap(remp,hist,231)
-    
-            cum_diff_Short.append(cummulative_difference(hist_map,reweight_map))
-            cum_diff_Long.append(cummulative_difference(true_hist,reweight_map))
-    
-        plt.plot(np.arange(stpsmin,stpsmax,1),cum_diff_Short);
-        plt.plot(np.arange(stpsmin,stpsmax,1),cum_diff_Long)
-    return cum_diff_Short,cum_diff_Long
-window_cumDiff(1)
+                cum_diff_Short[tau][stp] = -1
+                cum_diff_Long[tau][stp] = -1
+
+    # plt.plot(np.arange(stpsmin,stpsmax,1),cum_diff_Short);
+    # plt.plot(np.arange(stpsmin,stpsmax,1),cum_diff_Long)
+    cum_diff_Long.to_csv("Long_diffs.csv")
+    cum_diff_Short.to_csv("Short_diffs.csv")
+    # return cum_diff_Short,cum_diff_Long
 
 def Get_Lag_data(tau):
     data = pd.read_csv("/home/liam/lms464/CG/data/states/low_short_inactive_tmpNEW.csv",index_col=0)#low_long_inactive_tmpNEW.csv",index_col=0)#"~/CG/data/states/low_short_inactive_tmp.csv",index_col=0)
@@ -138,4 +186,7 @@ def Get_Lag_data(tau):
     reweigth_out.T.to_csv("~/lms464/CG/data/pi_eq_low_inactive_short_cgtmp_iter.csv")
 
 # Get_Lag_data(205)
-# short,long = lag_cumDiff(1,201)
+# short,long = 
+# lag_cumDiff(1,50)
+# window_cumDiff(1,50)
+compair_Lag_window_cumDiff(1,50,1,50)
